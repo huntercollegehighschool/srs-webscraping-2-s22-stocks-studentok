2f35bd61ac5d56ff461ce0577e7838a59e8a5e18
studentok <74572678+studentok@users.noreply.github.com>
2022-05-27 16:29:57 +0000

    completed

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import matplotlib.pyplot as plt
 
 def parsePrice(STOCK_NAME):
     r = requests.get('https://www.marketwatch.com/investing/stock/'+STOCK_NAME+'?mod=search_symbol') #returns html of the website
-    #print("r", r.text)
     soup = BeautifulSoup(r.text, 'html.parser') #use beautiful soup to parse the html; lxml
-    #print("soup:", soup.title.text)
-    
-    return price
+    price = soup.find('div', {'class':'intraday__data'}).find_all("bg-quote")[0].text
+    return(price)
 
-price = parsePrice("cost").replace(",", '')
+price = parsePrice("aapl").replace(",", '')
 print(price)
